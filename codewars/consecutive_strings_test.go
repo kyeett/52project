5d69243f682c8f5c7a53a26c500d612d0a48dc6d
Magnus Wahlstrand <magnus.wahlstrand@gmail.com>
2018-03-28 20:48:14 +0200

    Passing Consecutive Strings in Codewars

---
+++ codewars/consecutive_strings_test.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"strings"
+	"testing"
+
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+func LongestConsec(strarr []string, k int) string {
+	l := -1
+	maxS := ""
+	for i := 0; i < len(strarr)-k+1; i++ {
+		s := strings.Join(strarr[i:i+k], "")
+		if len(s) > l {
+			maxS = s
+			l = len(s)
+		}
+	}
+	return maxS
+}
+
+func dotest(strarr []string, k int, exp string) {
+	var ans = LongestConsec(strarr, k)
+	Expect(ans).To(Equal(exp))
+}
+
+func TestCart(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Shopping Cart Suite")
+}
+
+var _ = Describe("Test Example", func() {
+
+	It("should handle basic cases", func() {
+		dotest([]string{"zone", "abigail", "theta", "form", "libe", "zas"}, 2, "abigailtheta")
+		dotest([]string{"ejjjjmmtthh", "zxxuueeg", "aanlljrrrxx", "dqqqaaabbb", "oocccffuucccjjjkkkjyyyeehh"}, 1,
+			"oocccffuucccjjjkkkjyyyeehh")
+		dotest([]string{}, 3, "")
+		dotest([]string{"itvayloxrp", "wkppqsztdkmvcuwvereiupccauycnjutlv", "vweqilsfytihvrzlaodfixoyxvyuyvgpck"}, 2,
+			"wkppqsztdkmvcuwvereiupccauycnjutlvvweqilsfytihvrzlaodfixoyxvyuyvgpck")
+	})
+})
